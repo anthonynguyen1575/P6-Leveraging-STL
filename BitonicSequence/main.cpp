ef262340d0b0d179971ff6ff4d94b1d7fb618db0
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-12-16 21:41:49 -0800

    Update main.cpp

--- BitonicSequence/main.cpp
+++ BitonicSequence/main.cpp
@@ -5,10 +5,29 @@
 #include "Utility.h"
 #include "Car.h"
 
+std::vector<int> bitonicSequence(std::vector<int> list);
+
 int main() {
     auto integers = randomIntegers();
     std::cout << "Initial: " << integers << "\n";
 
-    // TODO: make the sequence a bitonic sequence
-    std::cout << "Bitonic: " << "TODO" << "\n";
+    std::vector<int> list = bitonicSequence(integers);
+    
+    std::cout << "Bitonic: " << list << "\n";
+}
+
+std::vector<int> bitonicSequence(std::vector<int> list) {
+    int halfWay = list.size() / 2;
+    std::vector<int> firstHalf(halfWay);
+    std::vector<int> secondHalf(list.size() - halfWay);
+
+    std::copy(list.begin(), list.begin() + halfWay, firstHalf.begin());
+    std::copy(list.begin() + halfWay, list.end(), secondHalf.begin());
+
+    std::sort(firstHalf.begin(), firstHalf.end());
+    std::sort(secondHalf.begin(), secondHalf.end(), std::greater<int>());
+
+    firstHalf.insert(firstHalf.end(), secondHalf.begin(), secondHalf.end());
+
+    return firstHalf;
 }
