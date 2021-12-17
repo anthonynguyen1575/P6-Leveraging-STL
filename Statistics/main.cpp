2f905c719faf492556d98ed71da48ab9c96e31ee
Anthony Nguyen <86622824+anthonynguyen1575@users.noreply.github.com>
2021-12-16 21:48:21 -0800

    Update main.cpp

--- Statistics/main.cpp
+++ Statistics/main.cpp
@@ -5,12 +5,47 @@
 #include "Utility.h"
 #include "Car.h"
 
+std::vector<double> statistics(std::vector<Car> cars);
+
 int main() {
     auto cars = randomCars();
     std::cout << "Cars: " << cars << "\n";
 
-    // TODO: print the average price and price range
-    std::cout << "The average price is: $" << "TODO" << "\n";
+    std::vector<double> values = statistics(cars);
 
-    std::cout << "The range is: $" << "TODO" << "\n";
+    std::cout << "The average price is: $" << values[0] << "\n";
+
+    std::cout << "The range is: $" << values[1] << "\n";
+}
+
+std::vector<double> statistics(std::vector<Car> cars) {
+    std::vector<double> values;
+    double total;
+    double averagePrice;
+    double range;
+    double min;
+    double max;
+
+    for(Car& it : cars) {
+        total += it.price();
+    }
+
+    averagePrice = total / cars.size();
+
+    min = cars[0].price();
+    for(Car& it : cars) {
+        if(it.price() < min) {
+            min = it.price();
+        }
+        if(it.price() > max) {
+            max = it.price();
+        }
+    }
+
+    range = max - min;
+
+    values.push_back(averagePrice);
+    values.push_back(range);
+
+    return values;
 }
